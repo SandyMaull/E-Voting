a8e8499a2c6352e53584a0846ca436aa21394278
SandyMaull <sandymaulana0@gmail.com>
2021-06-14 08:36:05 +0800

    New Push

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,6 +3,6 @@
       <strong>Copyright &copy; 2021 E-Voting UTS - Powered By: <a href="http://adminlte.io">AdminLTE.io.</a> & <a href="http://kuronekosan.web.id">Kuronekosan.</a></strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
-          <b><a href="https://github.com/SandyMaull/FT-E-Voting">Version</b> 1.0.2 Beta</a>
+          <b>Version</b> 1.0.2 Beta
       </div>
   </footer>
