40dd4607f566c8d575c5957259ed5de723047125
SandyMaull <sandymaulana0@gmail.com>
2021-06-16 13:16:45 +0800

    Bug Fixed

--- resources/views/admin/layouts/footer.blade.php
+++ resources/views/admin/layouts/footer.blade.php
@@ -3,6 +3,6 @@
       <strong>Copyright &copy; 2021 E-Voting UTS - Powered By: <a href="http://adminlte.io">AdminLTE.io.</a> & <a href="http://kuronekosan.web.id">Kuronekosan.</a></strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
-          <b>Version</b> 1.0.2 Beta
+          <b>Version</b> 1.0.3
       </div>
   </footer>
